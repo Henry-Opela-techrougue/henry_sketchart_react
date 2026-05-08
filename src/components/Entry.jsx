7b64833f4b768dd1c73e079863c2262838eeeca2
Henry <tokyorougue64@gmail.com>
2026-05-08 11:04:48 +0300

    sketch

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./Navbar";
+
 const Entry=() => {
 
     return (
+        
         <div className="row">
-            
+            <Navbar/>
+          <h1>Hello there user, welcome to Sketch Arts</h1>  
         </div>
     )
 }
